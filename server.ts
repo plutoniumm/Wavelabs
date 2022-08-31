e74d3fa46a4ca75b6a586e8aabffd75ba131f3e6
Manav <51614345+plutoniumm@users.noreply.github.com>
2022-08-31 09:12:06 +0530

    Server + TOC + CTX001_Init

--- server.ts
+++ server.ts
@@ -1,11 +1,44 @@
-// Bun Server
-const { serve, file, resolveSync } = Bun;
-// const { path } = import.meta;
+import { readFileSync } from "fs";
+import { serve, file } from "bun";
+import { marked } from 'marked';
 
 const PORT = process.env.PORT || 3000;
 
-console.log(`Server Running at http://localhost:${PORT}/`);
+const filter = (loc) => {
+    const name = loc.substring(1);
+    if (name.includes(".md")) {
+        const file = readFileSync(name, 'utf-8');
+        const parsed = marked.parse(file);
 
+        const cssed = parsed + `<link rel="stylesheet" href="https://sindresorhus.com/github-markdown-css/github-markdown-dark.css" crossorigin></head>` + // Adding Stylesheet
+            `<script>
+        document.body.classList.add("markdown-body");
+        </script>` + // Adding Script
+            `<style>
+        .markdown-body {
+            box-sizing: border-box;
+            min-width: 200px;
+            max-width: 980px;
+            margin: 0 auto;
+            padding: 45px;
+        }
+
+        @media (max-width: 767px) {
+            .markdown-body {
+                padding: 15px;
+            }
+        }
+        </style>`; // Adding Style limits
+
+        return new Response(cssed, {
+            headers: { 'content-type': 'text/html;charset=UTF-8' }
+        });
+    };
+
+    return new Response(file(name));
+};
+
+console.log(`Server Running at http://localhost:${PORT}/`);
 const server = serve({
     fetch ({ url }: Request) {
         const { pathname } = new URL(url);
@@ -17,12 +50,7 @@ const server = serve({
             return server.stop();
         }
 
-        return new Response(file(pathname.substring(1)));
+        return filter(pathname);
     },
-
-    // SSL is enabled if these two are set
-    // certFile: './cert.pem',
-    // keyFile: './key.pem',
-
     port: PORT, // number or string
 });
